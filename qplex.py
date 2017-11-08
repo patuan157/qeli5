00e2720cd05ad92d04d8d536fb46a1118e03788f
Vince Dang <xuanvu001@e.ntu.edu.sg>
2017-11-09 02:26:05 +0800

    Implement parser partially

--- qplex.py
+++ qplex.py
@@ -10,7 +10,7 @@ reserved = {
         'Unique': 'UNIQUE',
         'Append': 'APPEND',
         # Aggregate
-        'Aggregate': 'AGGREGATE'
+        'Aggregate': 'AGGREGATE',
         # Limit
         'Limit': 'LIMIT',
         # Join
@@ -23,7 +23,7 @@ reserved = {
         'Sort': 'SORT',
         'Key': 'KEY',
         # Group By
-        'HashAggregate': 'HASH_AGG'
+        'HashAggregate': 'HASH_AGG',
         'Group': 'GROUP',
         'BitmapAnd': 'BMP_AND',
         # Scan
@@ -41,9 +41,6 @@ reserved = {
         'Cond': 'CONDITION',
         'using': 'USING',
         # Operator
-        '<>': 'NOT_EQUAL',
-        '<=': 'LE',
-        '>=': 'GE',
         'AND': 'AND',
         'OR': 'OR',
         # Keywords
@@ -55,13 +52,19 @@ reserved = {
 
 tokens = [
         # KEYWORDS
+        'SUBOPS',
         'LPAREN',
         'RPAREN',
+        'TYPE',
         'COLON',
         'TO',
+        'IN',
 
         # OPERATORS
-        'EQUAL',
+        'NE',
+        'GE',
+        'LE',
+        'EQ',
         'LT',
         'GT',
         
@@ -72,17 +75,23 @@ tokens = [
         'LITERALS'
 ] + list(reserved.values())
 
+t_SUBOPS    = r'->'
 t_LPAREN    = r'\('
 t_RPAREN    = r'\)'
+t_TYPE      = r'\:\:'
 t_COLON     = r'\:'
 t_TO        = r'\.\.'
-t_EQUAL     = r'='
+t_IN        = r'\.'
+t_NE        = r'<>'
+t_LE        = r'<='
+t_GE        = r'>='
+t_EQ        = r'='
 t_LT        = r'<'
 t_GT        = r'>'
 t_LITERALS  = r'\'(\.|[^\'])*\''
 
 def t_ID(t):
-    r'[A-z][A-z_]*'
+    r'[A-z][A-z0-9_]*'
     t.type = reserved.get(t.value, 'ID')
     return t
 
@@ -117,64 +126,252 @@ def t_error(t):
 import ply.lex as lex
 lexer = lex.lex()
 
-while True:
-    string = input()
-    lexer.input(string)
-    while True:
-        tok = lexer.token()
-        if not tok:
-            break
-        print(tok)
+#while True:
+#    print('=======================')
+#    print('\n')
+#    string = input()
+#    lexer.input(string)
+#    while True:
+#        tok = lexer.token()
+#        if not tok:
+#            break
+#        print(tok)
 
 precedence = (
-    ('left', 'PLUS', 'MINUS'),
-    ('left', 'TIMES', 'DIVIDE'),
-    ('right', 'UMINUS')
+    ('left', 'OR'),
+    ('left', 'AND'),
 )
 
-names = {}
+def p_statement(p):
+    '''
+    statement : scan_stmt
+                | join_stmt
 
-def p_statement_assign(p):
-    'statement : NAME EQUALS expression'
-    names[p[1]] = p[3]
-
-def p_statement_expr(p):
-    'statement : expression'
-    print(p[1])
-
-def p_expression_binop(p):
-    '''expression : expression PLUS expression
-                    | expression MINUS expression
-                    | expression TIMES expression
-                    | expression DIVIDE expression'''
-    if (p[2] == '+'):
-        p[0] = p[1] + p[3]
-    elif (p[2] == '-'):
-        p[0] = p[1] - p[3]
-    elif (p[2] == '*'):
-        p[0] = p[1] * p[3]
-    elif (p[2] == '/'):
-        p[0] = p[1] / p[3]
-
-def p_expression_uminus(p):
-    'expression : MINUS expression %prec UMINUS'
-    p[0] = -p[2]
-
-def p_expression_group(p):
-    'expression : LPAREN expression RPAREN'
-    p[0] = p[2]
-
-def p_expression_number(p):
-    'expression : NUMBER'
+                | filter
+                | index_cond
+                | recheck
+                | summary
+                | predicate
+                | value
+                | empty
+    '''
     p[0] = p[1]
 
-def p_expression_name(p):
-    'expression : NAME'
-    try:
-        p[0] = names[p[1]]
-    except LookupError:
-        print('Undefined name "%s"' % p[1])
-        p[0] = 0
+def p_scan_stmt(p):
+    '''
+    scan_stmt : seq_scan_stmt
+                | index_scan_stmt
+                | index_only_scan_stmt
+                | bmp_scan_stmt
+    '''
+    p[0] = p[1]
+
+def p_join_stmt(p):
+    '''
+    join_stmt : nested_join_stmt
+                | hash_join_stmt
+                | merge_join_stmt
+    '''
+    p[0] = p[1]
+
+def p_seq_scan_stmt(p):
+    '''
+    seq_scan_stmt : seq_scan
+                | seq_scan filter
+    '''
+    print('Sequential Scan Statement')
+
+def p_index_scan_stmt(p):
+    '''
+    index_scan_stmt : index_scan
+                | index_scan index_cond
+    '''
+    print('Index Scan Statement')
+
+def p_index_only_scan_stmt(p):
+    '''
+    index_only_scan_stmt : index_only_scan
+                    | index_only_scan index_cond
+    '''
+    print('Index Only Scan Statement')
+
+def p_bmp_scan_stmt(p):
+    'bmp_scan_stmt : bmp_heap_scan_stmt SUBOPS bmp_scan_stmt_tail'
+    print('Bitmap Scan Statement')
+
+def p_bmp_scan_stmt_tail(p):
+    'bmp_scan_stmt_tail : bmp_index_scan_stmt'
+
+def p_bmp_heap_scan_stmt(p):
+    'bmp_heap_scan_stmt : bmp_heap_scan recheck'
+
+def p_bmp_index_scan_stmt(p):
+    '''
+    bmp_index_scan_stmt : bmp_index_scan 
+                    | bmp_index_scan index_cond
+    '''
+
+def p_nested_join_stmt(p):
+    '''
+    nested_join_stmt : nested_loop SUBOPS scan_stmt SUBOPS nested_join_stmt_tail
+    '''
+    print('Nested Loop Join')
+
+def p_nested_join_stmt_tail(p):
+    '''
+    nested_join_stmt_tail : scan_stmt
+                    | materialize SUBOPS scan_stmt
+    '''
+
+def p_hash_join_stmt(p):
+    'hash_join_stmt : hash_join hash_cond hash_join_stmt_tail'
+    print('Hash Join Statement')
+
+def p_hash_join_stmt_tail(p):
+    'hash_join_stmt_tail : SUBOPS scan_stmt SUBOPS hash_stmt'
+
+def p_hash_stmt(p):
+    'hash_stmt : hash scan_stmt'
+    print('Hash Statement')
+
+def p_merge_join_stmt(p):
+    'merge_join_stmt : merge_join merge_cond merge_join_stmt_tail'
+    print('Merge Join Statement')
+
+def p_merge_join_stmt_tail(p):
+    'merge_join_stmt_tail : SUBOPS sort_stmt SUBOPS sort_stmt'
+
+def p_sort_stmt(p):
+    'sort_stmt : sort sort_key SUBOPS statement'
+    print('Sort Statement')
+
+def p_seq_scan(p):
+    'seq_scan : SEQUENTIAL SCAN ON ID summary'
+    p[0] = p[1]
+    for i in range(2, 6):
+        p[0] += ',' + str(p[i])
+    print(p[0])
+
+def p_index_scan(p):
+    'index_scan : INDEX SCAN USING ID ON ID summary'
+    p[0] = p[1]
+    for i in range(2, 8):
+        p[0] += ',' + str(p[i])
+    print(p[0])
+
+def p_index_only_scan(p):
+    'index_only_scan : INDEX ONLY SCAN USING ID ON ID summary'
+    p[0] = p[1]
+    for i in range(2, 9):
+        p[0] += ',' + str(p[i])
+    print(p[0])
+
+def p_bmp_heap_scan(p):
+    'bmp_heap_scan : BITMAP HEAP SCAN ON ID summary'
+    p[0] = p[1]
+    for i in range(2, 7):
+        p[0] += ',' + str(p[i])
+    print(p[0])
+
+def p_bmp_index_scan(p):
+    'bmp_index_scan : BITMAP INDEX SCAN ON ID summary'
+    p[0] = p[1]
+    for i in range(2, 7):
+        p[0] += ',' + str(p[i])
+    print(p[0])
+
+def p_filter(p):
+    'filter : FILTER COLON predicate'
+    p[0] = p[1]
+    for i in range(2, 4):
+        p[0] += ',' + str(p[i])
+    print(p[0])
+
+def p_index_cond(p):
+    'index_cond : INDEX CONDITION COLON predicate'
+    p[0] = p[1]
+    for i in range(2, 5):
+        p[0] += ',' + str(p[i])
+    print(p[0])
+
+def p_recheck(p):
+    'recheck : RECHECK CONDITION COLON predicate'
+    p[0] = p[1]
+    for i in range(2, 5):
+        p[0] += ',' + str(p[i])
+    print(p[0])
+
+def p_summary(p):
+    'summary : LPAREN COST EQ COST_VAL TO COST_VAL ROWS EQ INT WIDTH EQ INT RPAREN'
+    p[0] = p[1]
+    for i in range(2, 14):
+        p[0] += ',' + str(p[i])
+
+def p_predicate(p):
+    '''
+    predicate : ID ops value
+                | LPAREN predicate RPAREN
+                | predicate AND predicate
+                | predicate OR predicate
+    '''
+    p[0] = p[1]
+    for i in range(2, 4):
+        p[0] += ',' + str(p[i])
+    print(p[0])
+
+def p_ops(p):
+    '''
+    ops : EQ
+        | GE
+        | LE
+        | GT
+        | LT
+        | NE
+    '''
+    p[0] = p[1]
+
+def p_value(p):
+    '''
+    value : INT
+            | COST_VAL
+            | object
+    '''
+    p[0] = p[1]
+
+def p_object(p):
+    'object : LITERALS TYPE ID'
+    p[0] = '(' + str(p[3]) + ') ' + str(p[1])
+
+def p_nested_loop(p):
+    'nested_loop : NESTED LOOP summary'
+
+def p_materialize(p):
+    'materialize : MATERIALIZE summary'
+
+def p_hash_join(p):
+    'hash_join : HASH JOIN summary'
+
+def p_hash(p):
+    'hash : HASH summary'
+
+def p_merge_join(p):
+    'merge_join : MERGE JOIN summary'
+
+def p_merge_cond(p):
+    'merge_cond : MERGE CONDITION COLON predicate'
+
+def p_sort(p):
+    'sort : SORT summary'
+
+def p_sort_key(p):
+    'sort_key : SORT KEY COLON ID IN ID'
+
+def p_hash_cond(p):
+    'hash_cond : HASH CONDITION COLON predicate'
+
+def p_empty(p):
+    'empty :'
+    pass
 
 def p_error(p):
     print('Syntax error at "%s"' % p.value)
@@ -184,7 +381,7 @@ parser = yacc.yacc()
 
 while 1:
     try:
-        s = input('cal > ')
+        s = input('qplex > ')
     except EOFError:
         break
     parser.parse(s)
