6a9a636ba274987f6a585af4e98dc81b4f5a068e
itsnhduc <nhduc.lhp@gmail.com>
2017-11-12 18:13:04 +0800

    Fix conn str bug

--- CustomFrame.py
+++ CustomFrame.py
@@ -38,7 +38,7 @@ class CustomFrame(MainFrame):
             dbHost = os.environ.get('DB_HOST')
             dbPwd = os.environ.get('DB_PWD')
             dbConnectionString = "dbname={} user={} host={}".format(dbName, dbUser, dbHost) + \
-                " password={}".format(dbPwd) if dbPwd is not None else ""
+                (" password={}".format(dbPwd) if dbPwd is not None else "")
             self.connection = database.connect(dbConnectionString)
         except Exception as err:
             print(err)
